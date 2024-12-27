c654181848f3db6a713b6a829889bab328bfb768
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2024-12-27 22:50:14 +0100

    added isAllowed method to user class

--- classes/User.php
+++ classes/User.php
@@ -86,4 +86,14 @@ class User{
     public function login(){
         
     }
+
+    public static function isAllowed($role){
+        if(isset($_SESSION['user_id']) && isset($_SESSION['role'])){
+            return $_SESSION['role'] == $role;
+        }else if($role == 'guest'){
+            return true;
+        }
+    
+        return false;
+    }
 }
